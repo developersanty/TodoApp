f7d84582798c21c086dc5388ccc330028004446e
Santhosh Jebamani <developersanty@gmail.com>
2017-04-11 15:19:32 -0500

    Added CRUD for a node

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Node.create(body: 'My first post')
+Node.create(body: 'My second post')
+Node.create(body: 'My third post')
+
